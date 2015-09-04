5cb91f700b83e33291dc8af6bcd20351a4ab41e3
Nick Conover <conover.nic@gmail.com>
2015-09-04 18:47:35 -0400

    save

---
+++ hello.java
@@ -0,0 +1,8 @@
+import skilstak.c;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println(c.clear + c.y + "Hello" + c.b + "world!" + c.x);
+   }
+}
+
---
+++ hi.java
@@ -0,0 +1,12 @@
+import skilstak.c;
+import java.util.Scanner;
+public class hi {
+    public static void main(String[] args) {
+        System.out.println(c.clear + c.y + "Hi, what's your name?"+ c.x);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("-->" + c.m);
+        String name = scanner.nextLine();
+        System.out.println("hello " + c.y + name + c.m);
+ }
+}
+
